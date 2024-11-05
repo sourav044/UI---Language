84e0e05028d1bc5ce33b1c4b25d8427e1bf194f1
Sourav Kumar <sourav044@hotmail.com>
2024-11-05 10:20:35 +0100

    dfhgdfg

--- ResxEditorApp/obj/Debug/net5.0-windows/ResxEditorApp.AssemblyInfo.cs
+++ ResxEditorApp/obj/Debug/net5.0-windows/ResxEditorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResxEditorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394eb4eefa907f2d5127a2c1c63889fcb2942bd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df78ebc823446adadfd351e7f9cbd58f536f482a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResxEditorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResxEditorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
